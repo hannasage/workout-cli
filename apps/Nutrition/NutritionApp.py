e7253eed6ac65a487182456928c8dae7e31ed8a5
Kevin Haube <booking@kevinmhaube.com>
2022-02-05 11:52:48 -0500

    MacroApp converted to NutritionApp and refactor (#5)

---
+++ apps/Nutrition/NutritionApp.py
@@ -0,0 +1,54 @@
+from apps.Nutrition.classes.Macros import Macros
+import pandas as pd
+
+class NutritionApp:
+
+    def __init__(self) -> None:
+        self.nutrition_log_path = 'C:\Projects\workout-cli\data\\nutrition-log.csv'
+        self.workout_log_path = 'C:\Projects\workout-cli\data\workouts-log.csv'
+
+    def create_entry(self):
+        # Read in and display current log tail and dtypes
+        log = pd.read_csv(self.nutrition_log_path, index_col=0)
+        workout_log = pd.read_csv(self.workout_log_path, index_col=0)
+
+        # Get workout relation
+        workout_id = check_workout_relation(workout_log)
+
+        # Collect new nutrition information
+        new_entry = Macros.create(workout_id)
+
+        # Convert Nutrition object into dataframe
+        entry_as_df = pd.DataFrame(Macros.to_dict(new_entry))
+        print(entry_as_df)
+
+        # Add new entry to nutrition_log
+        updated_log = pd.concat([log, entry_as_df]).reset_index(drop=True)
+
+        # Show new dataframe tail and confirm export 
+        print(updated_log.tail(10))
+
+        save = input('Overwrite old copy with new data? [y/n]: ')
+        if save == 'y' or save == 'Y':
+            updated_log.to_csv(self.nutrition_log_path)
+        else:
+            self.quit()
+
+    def quit(self):
+        print('Exiting, goodbye!')
+
+def check_workout_relation(log: pd.DataFrame):
+    print(log.tail())
+    workout_index = None
+    try:
+        workout_index = int(input('Which workout (by index) should we link it to? (RETURN to cont.): '))
+        return log.iloc[workout_index].id
+    except:
+        relation = input('Is this related to a workout? [y/n]: ')
+        if relation.lower() == 'y':
+            check_workout_relation(log)
+        else:
+            pass
+
+    return None
+        
---
+++ apps/Nutrition/classes/Macros.py
@@ -0,0 +1,95 @@
+
+from sqlite3 import Date
+from sqlite3 import DateFromTicks
+import pandas as pd
+
+from .Enums import *
+
+
+class Macros:
+
+    def __init__(
+            self,
+            workout_id,
+            cals,
+            protein,
+            carbs,
+            fiber,
+            sugars,
+            fats,
+            sat_fats,
+            unsat_fats,
+            chol,
+            sodium,
+            potassium
+        ) -> None:
+            self.workout_id = workout_id
+            self.date = Date.today()
+            self.cals = cals
+            self.protein = protein
+            self.carbs = carbs
+            self.fiber = fiber
+            self.sugars = sugars
+            self.fats = fats
+            self.sat_fats = sat_fats
+            self.unsat_fats = unsat_fats
+            # Convert from mg -> g
+            self.chol = int(chol) / 1000
+            self.sodium = int(sodium) / 1000
+            self.potassium = int(potassium) / 1000
+
+    # Enumerate class callsign
+    def callsign():
+        return 'nutrition'
+
+    # Converts to an item that pandas will turn into a DataFrame
+    def to_dict(self):
+        return {
+            'workout_id': [self.workout_id],
+            'date': [self.date],
+            'calories': [self.cals],
+            'protein': [self.protein],
+            'carbs': [self.carbs],
+            'fiber': [self.fiber],
+            'sugars': [self.sugars],
+            'fats': [self.fats],
+            'sat_fats': [self.sat_fats],
+            'unsat_fats': [self.unsat_fats],
+            'chol': [self.chol],
+            'sodium': [self.sodium],
+            'potassium': [self.potassium]
+        }
+    
+    # Prompts the user for the column name passed in
+    def prompt(column_header, measure):
+        return input(f'Enter {column_header} ({measure}): ')
+    
+    # Aggregates all the prompts into a single method call
+    @classmethod
+    def create(self, workout_id=None):
+        cals = self.prompt(NutritionEnums.CALS.value, Units.KCAL.value)
+        protein = self.prompt(NutritionEnums.PROTEIN.value, Units.GRAM.value)
+        carbs = self.prompt(NutritionEnums.CARBS.value, Units.GRAM.value)
+        fiber = self.prompt(NutritionEnums.FIBER.value, Units.GRAM.value)
+        sugars = self.prompt(NutritionEnums.SUGARS.value, Units.GRAM.value)
+        fats = self.prompt(NutritionEnums.FATS.value, Units.GRAM.value)
+        sat_fats = self.prompt(NutritionEnums.SAT_FATS.value, Units.GRAM.value)
+        unsat_fats = self.prompt(NutritionEnums.UNSAT_FATS.value, Units.GRAM.value)
+        chol = self.prompt(NutritionEnums.CHOL.value, Units.MG.value)
+        sodium = self.prompt(NutritionEnums.UNSAT_FATS.value, Units.MG.value)
+        potassium = self.prompt(NutritionEnums.UNSAT_FATS.value, Units.MG.value)
+        return Macros(
+            workout_id,
+            cals,
+            protein,
+            carbs,
+            fiber,
+            sugars,
+            fats,
+            sat_fats,
+            unsat_fats,
+            chol,
+            sodium,
+            potassium
+        )
+
